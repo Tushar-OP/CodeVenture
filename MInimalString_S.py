da60454cd4c6c8279a28c03d0103dca2cbdc9758
Shivani Singh <shivanisingh260500@gmail.com>
2020-05-19 19:15:11 +0530

    Add files via upload

---
+++ MInimalString_S.py
@@ -0,0 +1,12 @@
+s = input()
+a = [0]*26
+for i in s:
+    a[ord(i)-97] += 1
+t, u = [], []
+print(a)
+for i in s:
+    t.append(i)
+    a[ord(i)-97] -= 1
+    while t and sum(a[:ord(t[-1])-97]) == 0:
+        u.append(t.pop())
+print(''.join(u))
